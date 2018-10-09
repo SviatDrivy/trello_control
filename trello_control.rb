a38866a2510ebb7c8d6add4ce55349d51de6727e
Sviat <sviatoslav@drivy.com>
2018-10-09 22:06:18 +0200

    Creating new cards and desks in the trello

--- trello_control.rb
+++ trello_control.rb
@@ -16,6 +16,10 @@ end
 
 if ARGV[0] == "list"
  	puts infos
+elsif ARGV[0] == "create"
+	puts "new card created"
+	require_relative "secrets"
+	response = Faraday.post("https://api.trello.com/1/cards?", {"name" => ARGV[1], "desc" => ARGV[2], "idList" => "5bb39ee0029f335fb88aa2a9", "key" => API_KEY, "token" => API_TOKEN})
 else 
 	puts "error"
 end
